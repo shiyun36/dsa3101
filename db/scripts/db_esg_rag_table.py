b48524c94d60b50cfc84a483cc0c1cabe61db5fb
Alexandra Morales Tolentino <alexxtm.at@gmail.com>
2025-03-23 15:21:21 +0800

    created new branch because previous branch having merge issues, this branch has the db functions

---
+++ db/scripts/db_esg_rag_table.py
@@ -0,0 +1,56 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import json
+
+def insert_esg_rag_table(df):
+    # Load environment variables
+    load_dotenv('.env')
+    
+    # Get DB connection parameters
+    db_name = os.getenv('db_name')
+    db_user = os.getenv('db_user')
+    db_port = os.getenv('db_port')
+    db_host = os.getenv('db_host')
+    db_password = os.getenv('db_password')
+
+    ## SupaBase DB ##
+    # db_url = os.getenv('DATABASE_URL')
+    # conn = psycopg2.connect(db_url)
+
+
+    # Connect to the database
+    conn = psycopg2.connect(
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port
+    )
+    
+    # Create cursor
+    cur = conn.cursor()
+
+    # Define INSERT query
+    query = '''
+        INSERT INTO esg_rag_table (
+            company, year, topic, extracted_values, final_score
+        ) VALUES (%s, %s, %s, %s, %s)
+    '''
+
+    # Insert row-by-row
+    for _, row in df.iterrows():
+        print(row)
+        cur.execute(query, (
+            row["company"],
+            int(row["year"]),
+            row["topic"],
+            json.dumps(row["extracted_values"]),  # Convert dict to JSON string
+            None if pd.isna(row["final_score"]) else float(row["final_score"])
+        ))
+
+    # Commit and close
+    conn.commit()
+    cur.close()
+    conn.close()
