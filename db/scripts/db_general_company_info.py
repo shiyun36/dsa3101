947981d1cbe6f7259bb237658cacc0d769319987
Alexandra Morales Tolentino <shirokumochan@gmail.com>
2025-04-01 21:59:27 +0800

    uncommented local db stuff on insert_company_info

--- db/scripts/db_general_company_info.py
+++ db/scripts/db_general_company_info.py
@@ -11,21 +11,21 @@ def insert_company_info(df): #data_frame or text
     load_dotenv('.env')
     
     #Get DB Params for Local DB
-    db_name = os.getenv('db_name')
-    db_user = os.getenv('db_user')
-    db_port = os.getenv('db_port')
-    db_host = os.getenv('db_host')
-    db_password = os.getenv('db_password')
-    conn = psycopg2.connect(
-        dbname=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port=db_port
-    )
+    # db_name = os.getenv('db_name')
+    # db_user = os.getenv('db_user')
+    # db_port = os.getenv('db_port')
+    # db_host = os.getenv('db_host')
+    # db_password = os.getenv('db_password')
+    # conn = psycopg2.connect(
+    #     dbname=db_name,
+    #     user=db_user,
+    #     password=db_password,
+    #     host=db_host,
+    #     port=db_port
+    # )
     ## SupaBase DB ##
-    # db_url = os.getenv('DATABASE_URL')
-    # conn = psycopg2.connect(db_url)
+    db_url = os.getenv('DATABASE_URL')
+    conn = psycopg2.connect(db_url)
 
 
 
